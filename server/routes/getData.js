212f215fff574c7cda1a536fa700923014be47a7
Vance M Mcgrady <68658084+VanceMcGrady@users.noreply.github.com>
2023-03-26 12:02:04 -0700

    having issues with node-fetch method - going to try axios but I may be able to just send an axios fetch request from the front-end and forego the epress API altogether

--- server/routes/getData.js
+++ server/routes/getData.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch')
+const fetch = import('node-fetch')
 
 
-router.get('/', (req, res, next)=> {
+router.get('/', async (req, res, next)=> {
   console.log('get working in getData.js')
 
-  const method = "GET"
-  const url = 'https://www.usbr.gov/uc/water/hydrodata/reservoir_data/919/json/17.json'
-  fetch()
+ 
   next()
 })
 
